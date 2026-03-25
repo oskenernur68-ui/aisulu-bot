83ac8d5e8829f80ef34781efe545bc4022b595b5
oskenernur68-ui <oskenernur68@gmail.com>
2026-03-26 04:04:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 # ─── CONFIG ──────────────────────────────────────────────────────────────────
 TOKEN = "8653795023:AAGHcRMyCJFDCfaIxheyc9iarL_TkGQ9uBk"
-AI_API_KEY = "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"
+AI_API_KEY ="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"
 OPENROUTER_URL   = "https://openrouter.ai/api/v1/chat/completions"
 OPENROUTER_MODEL = "google/gemini-2.0-flash-001"
 
